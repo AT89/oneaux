618b3ba960c0fd93faa5160f326dae3cfbd79ce6
Thaisun Green <Tlgreen16@gmail.COM>
2016-07-18 17:23:02 -0400

    modified add controller js file

--- js/playlists/user/add.controller.js
+++ js/playlists/user/add.controller.js
@@ -40,8 +40,8 @@
 
             console.log(vm.new_song);
             vm.new_song.$save({playlist_id: vm.playlist_id}).then(function() {
-                console.go("should redirect");
-                $state.go("Join");
+                console.log("should redirect");
+                $state.go("CurrentPlaylist");
             })
         }
     }

382afc879d1a046fe78c805afb3eb3463ec30c39
Thaisun Green <Tlgreen16@gmail.COM>
2016-07-19 09:10:17 -0400

    Working on playlist View

--- js/playlists/user/playlist.controller.js
+++ js/playlists/user/playlist.controller.js
@@ -9,24 +9,25 @@ angular
   ])
 function CurrentPlaylistFunction(SongFactory,PlaylistFactory,$stateParams){
     var vm = this;
-    vm.songs = SongFactory.query({playlist_id: vm.playlist_id});
-    console.log(vm.songs);
-    
     vm.playlist_id = $stateParams.id;
-    vm.get_song = new SongFactory({
-      "user": "Anh",
-      "playlist_id": vm.playlist_id,
-      "title": title,
-      "artist": artist,
-      "album": album,
-      "album_art": image_url,
-      "duration": duration,
-      "audio_url": preview,
-      "score": 0
-    })
-    vm.addToPlaylist = function(new_song){
-        console.log("hello")
-    }
+    vm.songs = SongFactory.query({playlist_id: vm.playlist_id});
+    console.dir(vm.songs);
+    console.log(vm.playlist_id)
+
+    // vm.get_song = new SongFactory({
+    //   "user": "Anh",
+    //   "playlist_id": vm.playlist_id,
+    //   "title": title,
+    //   "artist": artist,
+    //   "album": album,
+    //   "album_art": image_url,
+    //   "duration": duration,
+    //   "audio_url": preview,
+    //   "score": 0
+    // });
+    // vm.addToPlaylist = function(new_song){
+    //     console.log("hello")
+    // }
 }
 
 

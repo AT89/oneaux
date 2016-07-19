8609cb5598d8924af32621c7b80adc2e3831b72c
Thaisun Green <Tlgreen16@gmail.COM>
2016-07-19 08:43:02 -0400

    added more functionality

--- js/playlists/user/playlist.controller.js
+++ js/playlists/user/playlist.controller.js
@@ -10,7 +10,17 @@ angular
 function CurrentPlaylistFunction(SongFactory,PlaylistFactory,$stateParams){
     var vm = this;
     vm.playlist_id = $stateParams.id;
-
+    vm.get_song = new SongFactory({
+      "user": "Anh",
+      "playlist_id": vm.playlist_id,
+      "title": title,
+      "artist": artist,
+      "album": album,
+      "album_art": image_url,
+      "duration": duration,
+      "audio_url": preview,
+      "score": 0
+    })
     vm.addToPlaylist = function(new_song){
         console.log("hello")
     }
